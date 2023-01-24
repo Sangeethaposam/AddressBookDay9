8c41a7cfb23b2aad26e829224fe4c840251cef32
Sangeethaposam <120437076+Sangeethaposam@users.noreply.github.com>
2023-01-24 11:25:20 +0530

    Here is the main method of address book

---
+++ AddressBookMain.java
@@ -0,0 +1,38 @@
+package com.bridzelabz.addressBook;
+import java.util.*;
+public class AddressBookMain {
+
+			public static void main(String[] args) {
+				AddressBook addressBook = new AddressBook();
+		        System.out.println("Welcome in AddressBook system");
+		        Scanner scanner = new Scanner(System.in);
+		        int operation;
+		        do {
+		            System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT \n3 EDIT \n4 Delete \n5. EXIT ");
+		            System.out.println("Enter the Operation Number");
+		            operation = scanner.nextInt();
+		            switch (operation) {
+		                case 1:
+		                    addressBook.addContact();
+		                    break;
+		                case 2:
+		                    System.out.println(addressBook);
+		                    break;
+		                case 3:
+		                    addressBook.editContact();
+		                    break;
+		                case 4:
+		                    addressBook.deleteContact();
+		                    break;
+		                case 5:
+		                    System.out.println("Exiting");
+		                    break;
+		                default:
+		                    System.out.println("Enter The Wrong Opration Number");
+		            }
+		        } while (operation != 5);
+		    }
+		}
+
+
+
